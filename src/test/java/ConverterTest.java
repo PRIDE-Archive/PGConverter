6dbaa3109d0d2332e145af9c2939426ba5531b3d
Tobias-Ternent <tobias@ebi.ac.uk>
2016-09-22 15:45:59 +0100

    Added unit tests for file format conversion and validation.

---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,37 @@
+/**
+ * Created by tobias on 22/09/2016.
+ */
+
+import org.junit.Test;
+import uk.ac.ebi.pride.toolsuite.pgconverter.Converter;
+import uk.ac.ebi.pride.toolsuite.pgconverter.MainApp;
+import uk.ac.ebi.pride.utilities.data.controller.impl.ControllerImpl.MzTabControllerImpl;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+import static uk.ac.ebi.pride.toolsuite.pgconverter.utils.Utility.*;
+
+public class ConverterTest {
+
+  /**
+   * This test converts an example mzIdentML file into an mzTab file.
+   * @throws Exception if there are problems opening the example file.
+   */
+  @Test
+  public void testConvertMzidToMztab() throws Exception{
+    URL url = ConverterTest.class.getClassLoader().getResource("test.mzid");
+    if (url == null) {
+      throw new IllegalStateException("no file for input found!");
+    }
+    File inputFile = new File(url.toURI());
+    File outputFile = File.createTempFile("test", ".mztab");
+    String[] args = new String[]{"-" + ARG_CONVERSION, "-" + ARG_INPUTFILE, inputFile.getPath(), "-" + ARG_OUTPUTFILE, outputFile.getPath()};
+    Converter.startConversion(MainApp.parseArgs(args));
+
+    MzTabControllerImpl mzTabController = new MzTabControllerImpl(outputFile);
+    mzTabController.close();
+    assertTrue("No errors reported during the conversion from  mzIdentML to MzTab", outputFile.exists());
+  }
+}
---
+++ src/test/java/ValidatorTest.java
@@ -0,0 +1,38 @@
+/**
+ * Created by tobias on 22/09/2016.
+ */
+
+import org.junit.Test;
+import uk.ac.ebi.pride.toolsuite.pgconverter.MainApp;
+import uk.ac.ebi.pride.toolsuite.pgconverter.Validator;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+import static uk.ac.ebi.pride.toolsuite.pgconverter.utils.Utility.*;
+
+public class ValidatorTest {
+
+  /**
+   * This test validated one example mzIdentML file which is related to a single peak .mgf file.
+   * @throws Exception if there are problems opening the example file.
+   */
+  @Test
+  public void testMzidValidator() throws Exception{
+    URL url = ConverterTest.class.getClassLoader().getResource("test.mzid");
+    if (url == null) {
+      throw new IllegalStateException("no file for input found!");
+    }
+    File inputMzidFile = new File(url.toURI());
+    url = ConverterTest.class.getClassLoader().getResource("test.mgf");
+    if (url == null) {
+      throw new IllegalStateException("no file for input found!");
+    }
+    File inputMgfFile = new File(url.toURI());
+    File reportFile = File.createTempFile("test", ".log");
+    String[] args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_MZID, inputMzidFile.getPath(), "-" + ARG_PEAK, inputMgfFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
+    Validator.startValidation(MainApp.parseArgs(args));
+    assertTrue("No errors reported during the conversion from  mzIdentML to MzTab", reportFile.exists());
+  }
+}
