19b397a4fe7e9e2c6a5ca161f99b4e3a55e95a1f
Tobias-Ternent <tobias@ebi.ac.uk>
2016-11-07 09:00:33 +0000

    Removed unused imports.

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,6 +1,4 @@
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.ac.ebi.pride.toolsuite.pgconverter.MainApp;
 import uk.ac.ebi.pride.toolsuite.pgconverter.Validator;
 
