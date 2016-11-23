c1d6766cea0d2105dc919b11e66a42656ab34a51
Tobias-Ternent <tobias@ebi.ac.uk>
2016-11-23 15:51:51 +0000

    Added 'bad' mzid for xml schema unit testing.

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -7,13 +7,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static uk.ac.ebi.pride.toolsuite.pgconverter.Validator.SCHEMA_OK_MESSAGE;
 import static uk.ac.ebi.pride.toolsuite.pgconverter.utils.Utility.*;
 
 /**
@@ -48,7 +47,7 @@ public class ValidatorTest {
   }
 
   /**
-   * This test performs xml schema and semantic validation on one example mzIdentML file which is related to a single peak .mgf file.
+   * This test performs xml schema-only validation on one example mzIdentML file which is related to a single peak .mgf file.
    *
    * @throws Exception if there are problems opening the example file.
    */
@@ -65,9 +64,24 @@ public class ValidatorTest {
     }
     File inputMgfFile = new File(url.toURI());
     File reportFile = File.createTempFile("testMzid", ".log");
-    String[] args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_SCHEMA_VALIDATION, "-" + ARG_MZID, inputMzidFile.getPath(), "-" + ARG_PEAK, inputMgfFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
+    String[] args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_SCHEMA_ONLY_VALIDATION, "-" + ARG_MZID, inputMzidFile.getPath(), "-" + ARG_PEAK, inputMgfFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
     Validator.startValidation(MainApp.parseArgs(args));
     assertTrue("No errors reported during the validation of the mzIdentML file", reportStatus(reportFile));
+    reportFile.delete();
+
+    url = ConverterTest.class.getClassLoader().getResource("mzidentml-example-bad.mzid");
+    if (url == null) {
+      throw new IllegalStateException("no file for input found!");
+    }
+    inputMzidFile = new File(url.toURI());
+    reportFile = File.createTempFile("testMzid", ".log");
+    args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_SCHEMA_ONLY_VALIDATION, "-" + ARG_MZID, inputMzidFile.getPath(), "-" + ARG_PEAK, inputMgfFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
+    Validator.startValidation(MainApp.parseArgs(args));
+    try {
+      reportStatus(reportFile);
+    } catch (IOException ioe) {
+      assertTrue("Errors reported during the validation of the mzIdentML file", ioe.toString().contains("ERROR"));
+    }
   }
 
   /**
@@ -89,19 +103,19 @@ public class ValidatorTest {
   }
 
   /**
-   * This test performs xml schema and semantic validation on one example PRIDE XML file.
+   * This test performs xml schema-only and semantic validation on one example PRIDE XML file.
    *
    * @throws Exception if there are problems opening the example file.
    */
   @Test
   public void testPridexmlSchemaValidator() throws Exception{
-    URL url = ConverterTest.class.getClassLoader().getResource("test.xml");
+    URL url = ConverterTest.class.getClassLoader().getResource("pride-example.xml");
     if (url == null) {
       throw new IllegalStateException("no file for input found!");
     }
     File inputPridexmlFile = new File(url.toURI());
     File reportFile = File.createTempFile("testPridexml", ".log");
-    String[] args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_SCHEMA_VALIDATION, "-" + ARG_PRIDEXML, inputPridexmlFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
+    String[] args = new String[]{"-" + ARG_VALIDATION, "-" + ARG_SCHEMA_ONLY_VALIDATION, "-" + ARG_PRIDEXML, inputPridexmlFile.getPath(), "-" + ARG_SKIP_SERIALIZATION, "-" + ARG_REPORTFILE , reportFile.getPath()};
     Validator.startValidation(MainApp.parseArgs(args));
     assertTrue("No errors reported during the validation of the PRIDE XML", reportStatus(reportFile));
   }
